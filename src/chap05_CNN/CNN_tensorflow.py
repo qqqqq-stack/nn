4215f6fd593fb1cc30ac43ac2163bcb78229fc09
qqqqq-stack <1349929289@qq.com>
2025-06-12 00:19:53 +0800

    Update CNN_tensorflow.py

--- src/chap05_CNN/CNN_tensorflow.py
+++ src/chap05_CNN/CNN_tensorflow.py
@@ -190,7 +190,7 @@ prediction = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
 # 交叉熵函数
 cross_entropy = tf.reduce_mean(
-    -tf.reduce_sum(ys * tf.log(prediction),reduction_indices=[1])
+    -tf.reduce_sum(ys * tf.log(prediction), axis=[1])
 )
 # 创建优化器 - Adam算法优化损失函数
 train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)

3eba828a948cdfb8c471b11a85054c1fb5b7a40e
qqqqq-stack <1349929289@qq.com>
2025-06-11 21:37:54 +0800

    Update linear_regression-tf2.0.py

--- src/chap02_linear_regression/linear_regression-tf2.0.py
+++ src/chap02_linear_regression/linear_regression-tf2.0.py
@@ -114,10 +114,9 @@ class linearModel(Model):
         return y
 
 
-    (xs, ys), (o_x, o_y) = load_data("train.txt")    # 调用load_data函数      
-    ndim = xs.shape[1]  # 获取特征维度
-
-    model = linearModel(ndim=ndim)  # 实例化线性模型
+(xs, ys), (o_x, o_y) = load_data("train.txt")    # 调用load_data函数      
+ndim = xs.shape[1]  # 获取特征维度
+model = linearModel(ndim=ndim)  # 实例化线性模型
 
 
 #训练以及评估
